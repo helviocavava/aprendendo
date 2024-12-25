11b27d32901df747242d5f5cca3e1ec6d1316e6a
helviocavava <helvio.d.f.c@gmail.com>
2024-12-25 08:16:22 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>se você está lendo isso, é porque você é importante pra mim e ta vendo o esforço que eu to fazendo pra eu mudar de vida.</h1>
+}
+
+export  default Home;
