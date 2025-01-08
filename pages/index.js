0623c8188b19bf036db123e45abe297e2a554fc0
helviocavava <helvio.d.f.c@gmail.com>
2025-01-08 06:12:07 +0000

    Adiciona `index.js`

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ function Home() {
   return (
     <h1>
       se você está lendo isso, é porque você é importante pra mim e ta vendo o
-      esforço que eu to fazendo pra eu mudar de vida.
+      esforço que eu estou fazendo para eu mudar de vida.
     </h1>
   );
 }
